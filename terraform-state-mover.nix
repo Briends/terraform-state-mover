54f29729a45d049634732967042875e39dff4b86
Jan-Philip Loos <maxdaten@gmail.com>
2023-08-27 20:04:33 +0200

    feat: Setup devShell and apps

--- terraform-state-mover.nix
+++ terraform-state-mover.nix
@@ -6,8 +6,18 @@
       src = ./.;
       evalSystem = "aarch64-darwin";
       compiler-nix-name = "ghc928";
+      shell.tools = {
+        cabal = {};
+        hlint = {};
+        haskell-language-server = {};
+      };
+      shell.buildInputs = with pkgs; [
+        alejandra
+      ];
     };
   flake = project.flake {};
 in {
+  apps.default = flake.apps."terraform-state-mover:exe:terraform-state-mover";
   packages.default = flake.packages."terraform-state-mover:exe:terraform-state-mover";
+  devShells = flake.devShells;
 }
