f6b48bc29e0c837ad7356834662535f465b165bb
Jan-Philip Loos <maxdaten@gmail.com>
2023-09-05 04:58:49 +0200

    feat: Switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   description = "terraform-state-mover";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = inputs: let
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }: let
     overlay = final: prev: {
       haskell =
         prev.haskell
@@ -17,27 +21,49 @@
         };
       terraform-state-mover = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.terraform-state-mover;
     };
-    perSystem = system: let
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [overlay];
-      };
-      hspkgs = pkgs.haskellPackages;
-    in {
-      devShells.default = hspkgs.shellFor {
-        withHoogle = true;
-        packages = p: [p.terraform-state-mover];
-        buildInputs = [
-          hspkgs.cabal-install
-          hspkgs.haskell-language-server
-          hspkgs.hlint
-          hspkgs.ormolu
-          pkgs.bashInteractive
-          pkgs.terraform
-        ];
-      };
-      packages.default = pkgs.terraform-state-mover;
-    };
   in
-    {overlays.default = overlay;} // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        hspkgs = pkgs.haskellPackages;
+      in {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        };
+
+        devShells.default = hspkgs.shellFor {
+          withHoogle = true;
+          packages = p: [p.terraform-state-mover];
+          buildInputs = [
+            hspkgs.cabal-install
+            hspkgs.haskell-language-server
+            hspkgs.hlint
+            hspkgs.ormolu
+            pkgs.bashInteractive
+            pkgs.terraform
+          ];
+        };
+        packages.default = pkgs.terraform-state-mover;
+
+        apps.default = {
+          program = pkgs.terraform-state-mover;
+        };
+
+        overlays.default = overlay;
+      };
+
+      flake = {
+      };
+    };
 }
